423133627bd223c04a5e077252c845e6d492fb87
Julian Jark <juliuxu@gmail.com>
2022-05-13 15:19:44 +0200

    add link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,24 +6,10 @@ export default function Index() {
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            href="/ladle"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            Open ladle
           </a>
         </li>
       </ul>
